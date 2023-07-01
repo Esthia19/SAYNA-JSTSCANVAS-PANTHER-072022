99b782798bd3bd8f81eeaa1e4466a01cb2404dd8
esthia 19 <esthiaandriamalala0@gmail.com>
2023-07-01 16:43:20 +0200

    init

---
+++ js/landing.js
@@ -0,0 +1,26 @@
+var element = document.querySelectorAll('.milieu1');
+element.forEach(function(element){
+    element.classList.add('slide-in-left'); 
+    element.style.visibility='visible'
+});
+
+var element = document.querySelectorAll('.milieu2');
+element.forEach(function(element){
+    element.classList.add('slide-in-left'); 
+    element.style.visibility='visible'
+});
+
+var element = document.querySelectorAll('.milieu3');
+element.forEach(function(element){
+    element.classList.add('slide-in-left'); 
+    element.style.visibility='visible'
+});
+
+
+var element = document.querySelectorAll('.formulaire');
+element.forEach(function(element){
+    element.classList.add('slide-in-left'); 
+    element.style.visibility='visible'
+});
+
+
